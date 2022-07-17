41747d833652df688eccecf5b597bc234a9c632c
S. Jansen <sa.jansen1@gmail.com>
2022-07-17 18:23:50 +0200

    dual core test

--- main/main.c
+++ main/main.c
@@ -2,12 +2,34 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+int global = 0;
 
+void gui(void* args) {
+    printf("initizizing GUI...\n");
+    while (1) {
+        global++;
+        printf("[ ] Hello world by GUI! Global: %d\n", global);
+        vTaskDelay(3000 / portTICK_PERIOD_MS);
+    }
+    vTaskDelete(NULL);
+}
+
+// Running on main core
 void app_main(void)
 {
+
+    portBASE_TYPE result = xTaskCreatePinnedToCore(&gui, "gui",
+                            3584 + 512, NULL,
+                                                   0, NULL, 1);
+
+    if (result != pdTRUE) {
+        printf("Failed to create GUI task");
+    }
+
     int i = 0;
     while (1) {
-        printf("[%d] Hello world!\n", i);
+        global++;
+        printf("[%d] Hello world! Global: %d\n", i, global);
         i++;
         vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
