4686cfa3f91106f23b6d3def93c455b731ec936a
S. Jansen <sa.jansen1@gmail.com>
2022-12-07 10:41:57 +0200

    fix: Fixed release script

--- release.sh
+++ release.sh
@@ -20,14 +20,14 @@ HEREDOC
 function setVersion() {
     version="$1"
 
-    sed -i -e "s/APP_VERSION = \".*\";/APP_VERSION = \"${version}\";/g" ./main/app_version.h
+    sed -i -e "s/APP_VERSION = \".*\";/APP_VERSION = \"${version}\";/g" ./main/version.h
 }
 
 function releasePatch {
   git checkout master || exit 1
 
   # Create patch version
-  CURRENT_VERSION=$(cat ./main/app_version.h | grep "APP_VERSION = \"" | awk -F'"' '{print $2}')
+  CURRENT_VERSION=$(cat ./main/version.h | grep "APP_VERSION = \"" | awk -F'"' '{print $2}')
   RELEASE_VERSION=$(echo ${CURRENT_VERSION} | sed 's/v//g' | awk -F'.' '{print $1"."$2"."$3+1}')
 
   git merge develop || exit 1
@@ -42,7 +42,7 @@ function releaseMinor {
   git merge develop || exit 1
 
   # Create patch version
-  CURRENT_VERSION=$(cat ./main/app_version.h | grep "APP_VERSION = \"" | awk -F'"' '{print $2}')
+  CURRENT_VERSION=$(cat ./main/version.h | grep "APP_VERSION = \"" | awk -F'"' '{print $2}')
   RELEASE_VERSION=$(echo ${CURRENT_VERSION} | awk -F'.' '{print $1"."$2".0"}')
 
   setVersion "${RELEASE_VERSION}" || exit 1
@@ -55,7 +55,7 @@ function releaseMajor {
   git merge develop || exit 1
 
   # Create patch version
-  CURRENT_VERSION=$(cat ./main/app_version.h | grep "APP_VERSION = \"" | awk -F'"' '{print $2}')
+  CURRENT_VERSION=$(cat ./main/version.h | grep "APP_VERSION = \"" | awk -F'"' '{print $2}')
   RELEASE_VERSION=$(echo ${CURRENT_VERSION} | awk -F'.' '{print $1+1".0.0"}')
 
   setVersion "${RELEASE_VERSION}" || exit 1
@@ -67,7 +67,7 @@ function pushAndRelease {
   RELEASE_VERSION="$1"
   echo "Release version: ${RELEASE_VERSION}"
 
-  git add main/app_version.h || exit 1
+  git add main/version.h || exit 1
   git commit -m "version release: ${RELEASE_VERSION}" || exit 1
   git tag "v${RELEASE_VERSION}" || exit 1
   git push -u origin master --tags || exit 1
@@ -78,13 +78,13 @@ function setNextDevelopmentVersion {
   git rebase master || exit 1
 
   # Generate next (minor) development version
-  CURRENT_VERSION=$(cat ./main/app_version.h | grep "APP_VERSION = \"" | awk -F'"' '{print $2}')
+  CURRENT_VERSION=$(cat ./main/version.h | grep "APP_VERSION = \"" | awk -F'"' '{print $2}')
   DEV_VERSION=$(echo ${CURRENT_VERSION} | sed 's/v//g' | awk -F'.' '{print $1"."$2+1".0-SNAPSHOT"}')
 
   echo "Next development version: ${DEV_VERSION}"
   setVersion "${DEV_VERSION}" || exit 1
 
-  git add main/app_version.h || exit 1
+  git add main/version.h || exit 1
   git commit -m "next development version" || exit 1
   git push -u origin develop --tags || exit 1
 }
