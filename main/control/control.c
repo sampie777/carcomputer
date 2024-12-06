79b94af24b61b5e922d9b317f805213728ee98c6
S. Jansen <samuelanton@hotmail.com>
2024-12-06 18:25:36 +0200

    feat(buttons): Enabled main menu

--- main/control/control.c
+++ main/control/control.c
@@ -5,6 +5,8 @@
 #include <math.h>
 #include "../config.h"
 #include "control.h"
+
+#include "buttons.h"
 #include "../peripherals/canbus/canbus.h"
 #include "../peripherals/mpu9250.h"
 #include "../return_codes.h"
@@ -34,117 +36,16 @@ void control_read_analog_sensors(State* state) {
     mpu9250_read(state);
 }
 
-typedef enum {
-    PidProportional = 0,
-    PidIntegral,
-    PidDerivative,
-} PidIncreaseTarget;
 
 void control_read_user_input(State* state) {
     static int64_t last_read_time = 0;
-    static double pid_increase_step = 0.01;
-    static PidIncreaseTarget pid_increase_target = PidProportional;
 
     if (esp_timer_get_time_ms() < last_read_time + BUTTONS_READ_INTERVAL_MS) return;
     last_read_time = esp_timer_get_time_ms();
 
     Button button = buttons_get_pressed();
-    switch (button) {
-        case BUTTON_NONE:
-            break;
-        case BUTTON_UP:
-            printf("Button pressed: BUTTON_UP\n");
-            state->cruise_control.enabled = true;
-            break;
-        case BUTTON_VOLUME_UP:
-            printf("Button pressed: BUTTON_VOLUME_UP\n");
-            if (!state->cruise_control.enabled) {
-                if (pid_increase_target == PidProportional) {
-                    state->cruise_control.pidKp += pid_increase_step;
-                    printf("state->cruise_control.pidKp = %lf\n", state->cruise_control.pidKp);
-                } else if (pid_increase_target == PidIntegral) {
-                    state->cruise_control.pidKi += pid_increase_step;
-                    printf("state->cruise_control.pidKi = %lf\n", state->cruise_control.pidKi);
-                } else if (pid_increase_target == PidDerivative) {
-                    state->cruise_control.pidKd += pid_increase_step;
-                    printf("state->cruise_control.pidKd = %lf\n", state->cruise_control.pidKd);
-                }
-                break;
-            }
-
-            state->cruise_control.target_speed++;
-            break;
-        case BUTTON_VOLUME_DOWN:
-            printf("Button pressed: BUTTON_VOLUME_DOWN\n");
-            if (!state->cruise_control.enabled) {
-                if (pid_increase_target == PidProportional) {
-                    state->cruise_control.pidKp -= pid_increase_step;
-                    printf("state->cruise_control.pidKp = %lf\n", state->cruise_control.pidKp);
-                } else if (pid_increase_target == PidIntegral) {
-                    state->cruise_control.pidKi -= pid_increase_step;
-                    printf("state->cruise_control.pidKi = %lf\n", state->cruise_control.pidKi);
-                } else if (pid_increase_target == PidDerivative) {
-                    state->cruise_control.pidKd -= pid_increase_step;
-                    printf("state->cruise_control.pidKd = %lf\n", state->cruise_control.pidKd);
-                }
-                break;
-            }
-
-            state->cruise_control.target_speed--;
-            if (state->cruise_control.target_speed < 0) state->cruise_control.target_speed = 0;
-            break;
-        case BUTTON_SOURCE:
-            printf("Button pressed: BUTTON_SOURCE\n");
-            if (state->cruise_control.enabled) printf("Disconnecting cruise control because of user input\n");
-            state->cruise_control.enabled = false;
-            break;
-        case BUTTON_SOURCE_LONG_PRESS:
-            printf("Button pressed: BUTTON_SOURCE_LONG_PRESS\n");
-            if (pid_increase_target == PidProportional) {
-                pid_increase_target = PidIntegral;
-                printf("pid_increase_target = PidIntegral\n");
-            } else if (pid_increase_target == PidIntegral) {
-                pid_increase_target = PidDerivative;
-                printf("pid_increase_target = PidDerivative\n");
-            } else {
-                pid_increase_target = PidProportional;
-                printf("pid_increase_target = PidProportional\n");
-            }
-
-        // utils_reboot(state);
-            break;
-        case BUTTON_INFO: printf("Button pressed: BUTTON_INFO\n");
-            break;
-        case BUTTON_DOWN: printf("Button pressed: BUTTON_DOWN\n");
-            break;
-        case BUTTON_VOLUME_UP_LONG_PRESS: printf("Button pressed: BUTTON_VOLUME_UP_LONG_PRESS\n");
-
-            if (!state->cruise_control.enabled) {
-                pid_increase_step *= 10;
-                return;
-            }
-
-            state->cruise_control.target_speed += 10 - 1;
-            break;
-        case BUTTON_VOLUME_DOWN_LONG_PRESS: printf("Button pressed: BUTTON_VOLUME_DOWN_LONG_PRESS\n");
-            if (!state->cruise_control.enabled) {
-                pid_increase_step *= 0.1;
-                return;
-            }
-
-            state->cruise_control.target_speed -= 10 - 1;
-            if (state->cruise_control.target_speed < 0) state->cruise_control.target_speed = 0;
-            break;
-        case BUTTON_INFO_LONG_PRESS: printf("Button pressed: BUTTON_INFO_LONG_PRESS\n");
-            break;
-        case BUTTON_UP_LONG_PRESS: printf("Button pressed: BUTTON_UP_LONG_PRESS\n");
-            state->cruise_control.target_speed = state->cruise_control.previous_target_speed;
-            break;
-        case BUTTON_DOWN_LONG_PRESS: printf("Button pressed: BUTTON_DOWN_LONG_PRESS\n");
-            break;
-        default:
-            break;
-    }
+    control_buttons_handle(state, button);
+    control_buttons_handle_pid_config(state, button);
 }
 
 void control_led_indicator_step(State* state) {
@@ -236,9 +137,11 @@ void control_mpu_power(State* state) {
     ignition_off_time = 0;
 }
 
-void control_crash_detection(State *state) {
+void control_crash_detection(State* state) {
     static int64_t last_sent = 0;
-    double total_force = sqrt(state->motion.accel_x * state->motion.accel_x + state->motion.accel_y * state->motion.accel_y + state->motion.accel_z * state->motion.accel_z);
+    double total_force = sqrt(
+        state->motion.accel_x * state->motion.accel_x + state->motion.accel_y * state->motion.accel_y + state->motion.
+        accel_z * state->motion.accel_z);
     if (total_force < CRASH_DETECTION_CRASH_MIN_G) return;
 
     if (esp_timer_get_time_ms() < last_sent + CRASH_DETECTION_CRASH_MAX_DURATION_MS) return;
