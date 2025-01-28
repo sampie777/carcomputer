4630b52db4eac0cea14352ae5873a13a49d36672
S. Jansen <samuelanton@hotmail.com>
2025-01-28 13:35:36 +0200

    refactor(gui): Renamed sh1106 to display to make it more convienent.

--- main/peripherals/display/display_screens.h
+++ main/peripherals/display/display_screens.h
@@ -8,18 +8,18 @@
 #include "../../state.h"
 #include "sh1106.h"
 
-void content_cruise_control(State* state, SH1106Config* sh1106);
+void content_main_menu(const State* state, SH1106Config* display);
 
-void content_power_off_count_down(State* state, SH1106Config* sh1106);
+void content_cruise_control(State* state, SH1106Config* display);
 
-void content_motion_sensors_data(const State* state, SH1106Config* sh1106);
+void content_power_off_count_down(State* state, SH1106Config* display);
 
-void content_actions(const State* state, SH1106Config* sh1106);
+void content_motion_sensors_data(const State* state, SH1106Config* display);
 
-void content_location_data(const State* state, SH1106Config* sh1106);
+void content_actions(const State* state, SH1106Config* display);
+
+void content_location_data(const State* state, SH1106Config* display);
 
 void content_error_codes(const State* state, SH1106Config* display);
-
-void content_main_menu(const State* state, SH1106Config* sh1106);
 
 #endif //CARCOMPUTER_DISPLAY_SCREENS_H
